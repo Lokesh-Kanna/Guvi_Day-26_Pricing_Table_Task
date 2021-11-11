afcfa6d8492e1f63254877c5245ce26b04a362c3
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-11 16:40:00 +0530

    Separated components to different files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import CheckIcon from '@mui/icons-material/Check';
-import ClearIcon from '@mui/icons-material/Clear';
+import { Distributer } from './Distributer';
 
 function App() {
   const cardContent = [
@@ -51,87 +45,6 @@ function App() {
       <Distributer cardContent={cardContent} />
     </div>
   );
-}
-
-function Distributer({ cardContent }) {
-  return (
-      <div className="content">
-        {cardContent.map((cont, index) => {
-          return (
-            <CardDisp 
-                option = {cont.option}
-                price = {cont.price}
-                user = {cont.user}
-                storage = {cont.storage}
-                publicprojects = {cont.publicprojects}
-                access = {cont.access}
-                privateprojects = {cont.privateprojects}
-                support = {cont.support}
-                subdomain = {cont.subdomain}
-                statusreport = {cont.statusreport}
-                id = {cont.index}
-                />
-          )
-        })}
-      </div>
-  )
-}
-
-
-
-function CardDisp({option, price, user, storage, publicprojects, access, privateprojects, support, subdomain, statusreport, id}) {
-  const style1 = {color: option == "FREE" ? "lightgrey" : "black"}
-  const style2 = {color: option == "FREE" || option == "PLUS" ? "lightgrey" : "black"}
-  return (
-    <Card id="card" sx={{ maxWidth: 300 }}>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        {option}
-      </Typography>
-      <Typography variant="h5" component="div">
-        {price} <span id="subtxt">/month</span>
-      </Typography>
-      <hr></hr>
-      <div className="details">
-        <Typography className="typo">
-          <CheckIcon />
-            {user}
-          </Typography>
-        <Typography className="typo">
-          <CheckIcon />
-            {storage}
-          </Typography>
-        <Typography className="typo">
-          <CheckIcon />
-            {publicprojects}
-          </Typography>
-        <Typography className="typo">
-          <CheckIcon />
-            {access}
-          </Typography>
-        <Typography className="typo" style={style1}>
-        {option == "FREE" ? <ClearIcon /> : <CheckIcon />}
-            {privateprojects}
-          </Typography>
-        <Typography className="typo" style={style1}>
-        {option == "FREE" ? <ClearIcon /> : <CheckIcon />}
-            {support}
-          </Typography>
-        <Typography className="typo" style={style1}>
-        {option == "FREE" ? <ClearIcon /> : <CheckIcon />}
-            {subdomain}
-          </Typography>
-        <Typography className="typo" style={style2}>
-        {option == "FREE" || option == "PLUS"? <ClearIcon /> : <CheckIcon />}
-            {statusreport}
-          </Typography>
-      </div>
-    </CardContent>
-    <CardActions className="cardaction">
-      <Button id="btn" variant="contained">Button</Button>
-    </CardActions>
-  </Card>
-  )
 }
 
 export default App;
