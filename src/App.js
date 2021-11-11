d297147b166e54d5d07f601cec8fbad409244077
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-11 15:53:01 +0530

    Added all details in cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
 
 function App() {
+  const cardContent = [
+    {
+      option: "FREE",
+      price: "$0",
+      user: "Single User",
+      storage: "5GB Storage",
+      publicprojects: "Unlimited Public Projects",
+      access: "Community Access",
+      privateprojects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      statusreport: "Monthly Status Report"
+    },
+    {
+      option: "PLUS",
+      price: "$9",
+      user: "5 Users",
+      storage: "5GB Storage",
+      publicprojects: "Unlimited Public Projects",
+      access: "Community Access",
+      privateprojects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      statusreport: "Monthly Status Report"
+    },
+    {
+      option: "PRO",
+      price: "$49",
+      user: "Unlimited Users",
+      storage: "5GB Storage",
+      publicprojects: "Unlimited Public Projects",
+      access: "Community Access",
+      privateprojects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      statusreport: "Monthly Status Report"
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Distributer cardContent={cardContent} />
     </div>
   );
+}
+
+function Distributer({ cardContent }) {
+  return (
+      <div className="content">
+        {cardContent.map((cont, index) => {
+          return (
+            <CardDisp 
+                option = {cont.option}
+                price = {cont.price}
+                user = {cont.user}
+                storage = {cont.storage}
+                publicprojects = {cont.publicprojects}
+                access = {cont.access}
+                privateprojects = {cont.privateprojects}
+                support = {cont.support}
+                subdomain = {cont.subdomain}
+                statusreport = {cont.statusreport}
+                />
+          )
+        })}
+      </div>
+  )
+}
+
+
+
+function CardDisp({option, price, user, storage, publicprojects, access, privateprojects, support, subdomain, statusreport}) {
+  return (
+    <Card id="card" sx={{ maxWidth: 300 }}>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        {option}
+      </Typography>
+      <Typography variant="h5" component="div">
+        {price} <span id="subtxt">/month</span>
+      </Typography>
+      <hr></hr>
+      <div className="details">
+        <Typography className="typo">
+          <CheckIcon />
+            {user}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {storage}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {publicprojects}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {access}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {privateprojects}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {support}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {subdomain}
+          </Typography>
+        <Typography className="typo">
+          <CheckIcon />
+            {statusreport}
+          </Typography>
+      </div>
+    </CardContent>
+    <CardActions className="cardaction">
+      <Button id="btn" variant="contained">Button</Button>
+    </CardActions>
+  </Card>
+  )
 }
 
 export default App;
